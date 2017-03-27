e28e8a26bfd9943ab8a182efcdac8fef53541519
Sakai Aoi <SakaiAoi@Sakai-no-MacBook-Pro.local>
2017-03-27 18:54:56 +0900

    add color option

--- app/src/main/java/com/lifesitech/android/timetable/MainActivity.java
+++ app/src/main/java/com/lifesitech/android/timetable/MainActivity.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
         m1s.setText(sharedPreferences.getString("subject_m1", ""));
         m1t.setText(sharedPreferences.getString("teacher_m1", ""));
+        m1s.setBackgroundColor(getResources().getColor(sharedPreferences.getInt("color_m1", Color.WHITE)));
+        m1t.setBackgroundColor(getResources().getColor(sharedPreferences.getInt("color_m1", Color.WHITE)));
     }
 
 
@@ -51,9 +56,293 @@ public class MainActivity extends AppCompatActivity {
 
 
         mDialog = new Dialog(MainActivity.this);
+        mDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        mDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
 
         mDialog.setContentView(R.layout.dialog_custom);
-        mDialog.setTitle("Dialogのタイトル");
+
+        mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        mDialog.findViewById(R.id.color1).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color1);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color1));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color1));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color2).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color2);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color2));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color2));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color3).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color3);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color3));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color3));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color4).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color4);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color4));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color4));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color5).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color5);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color5));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color5));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color6).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color6);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color6));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color6));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color7).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color7);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color7));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color7));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color8).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color8);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color8));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color8));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color9).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color9);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color9));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color9));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color10).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color10);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color10));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color10));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color11).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color11);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color11));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color11));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color12).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color12);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color12));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color12));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color13).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color13);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color13));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color13));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color14).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color14);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color14));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color14));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color15).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color15);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color15));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color15));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color16).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color16);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color16));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color16));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color17).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color17);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color17));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color17));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color18).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color18);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color18));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color18));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color19).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color19);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color19));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color19));
+                    }
+                }
+        );
+
+        mDialog.findViewById(R.id.color20).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("color_m1", R.color.color20);
+                        editor.apply();
+                        m1s.setBackgroundColor(getResources().getColor(R.color.color20));
+                        m1t.setBackgroundColor(getResources().getColor(R.color.color20));
+                    }
+                }
+        );
+
         mDialog.findViewById(R.id.ok).setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {

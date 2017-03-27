a7239541e5ea46e6277f83c1d83499aeedafba83
Sakai Aoi <SakaiAoi@Sakai-no-MacBook-Pro.local>
2017-03-27 17:01:59 +0900

    add dialog

--- app/src/main/java/com/lifesitech/android/timetable/MainActivity.java
+++ app/src/main/java/com/lifesitech/android/timetable/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lifesitech.android.timetable;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -7,8 +8,10 @@ import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -23,8 +26,10 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     TextView m1s;
     TextView m1t;
-    EditText edit_subject;
     Map<String, String> map = new HashMap<>();
+    Dialog mDialog;
+    EditText edit_subject;
+    EditText edit_teacher;
 
 
 
@@ -34,38 +39,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         m1s = (TextView) findViewById(R.id.m1s);
         m1t = (TextView) findViewById(R.id.m1t);
+
         SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
         m1s.setText(sharedPreferences.getString("subject_m1", ""));
-        m1t.setText(sharedPreferences.getString("teacher_m2", ""));
+        m1t.setText(sharedPreferences.getString("teacher_m1", ""));
     }
 
 
     public void dialog_m1(View v) {
         //テキスト入力を受け付けるビューを作成します。
-        final EditText editView = new EditText(MainActivity.this);
-        final EditText editView2 = new EditText(MainActivity.this);
-        new AlertDialog.Builder(MainActivity.this)
-                .setIcon(android.R.drawable.ic_dialog_info)
-                .setTitle("テキスト入力ダイアログ")
-                //setViewにてビューを設定します。
-                .setView(editView)
-                .setView(editView2)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
+
+
+        mDialog = new Dialog(MainActivity.this);
+
+        mDialog.setContentView(R.layout.dialog_custom);
+        mDialog.setTitle("Dialogのタイトル");
+        mDialog.findViewById(R.id.ok).setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        //OKを押したときの処理
+                        edit_subject = (EditText) mDialog.findViewById(R.id.edit_subject);
+                        edit_teacher = (EditText) mDialog.findViewById(R.id.edit_teacher);
                         SharedPreferences sharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("subject_m1", editView.getText().toString());
-                        editor.putString("teacher_m1", editView2.getText().toString());
-                        m1s.setText(editView.getText().toString());
-
+                        editor.putString("subject_m1", edit_subject.getText().toString());
+                        editor.putString("teacher_m1", edit_teacher.getText().toString());
+                        editor.apply();
+                        m1s.setText(edit_subject.getText().toString());
+                        m1t.setText(edit_teacher.getText().toString());
+                        mDialog.dismiss();
                     }
-                })
-                .setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                    }
-                })
-                .show();
-
+                }
+        );
+        mDialog.show();
     }
 
     public void ok(View v) {
